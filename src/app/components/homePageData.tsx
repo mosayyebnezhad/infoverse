d42d9f4ecdbf7a4f174d5bd03dd5fe436b1be218
mosayyebnezhad <mohammadmosayyebnezhad@gmail.com>
2024-09-18 17:22:01 +0330

    work?001

--- src/app/components/homePageData.tsx
+++ src/app/components/homePageData.tsx
@@ -6,28 +6,28 @@ import { useFetch } from "../hooks/useFetch"
 
 export default async function HomePage() {
 
-    // const Data = await axios.get("https://fa.wikipedia.org/w/api.php", {
-    //     params: {
-    //         action: "query",
-    //         list: "random",
-    //         rnlimit: "8",
-    //         format: "json",
-    //         origin: "*",
-    //         rnnamespace: 0
-    //     }
-    // })
+    const Data = await axios.get("https://fa.wikipedia.org/w/api.php", {
+        params: {
+            action: "query",
+            list: "random",
+            rnlimit: "8",
+            format: "json",
+            origin: "*",
+            rnnamespace: 0
+        }
+    })
 
 
 
     return (
         <div>
-            {/* {Data.data.query.random.map((item: any) => (
+            {Data.data.query.random.map((item: any) => (
 
                 <Link href={`/${item.id}`}>
                     <h2 key={item.id}>{item.title}</h2>
         
                 </Link>
-            ))} */}
+            ))}
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,27 @@
-// app/page.tsx
-import axios from 'axios'
-import Link from 'next/link'
+
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { SearchIcon } from "lucide-react";
 import { useTheme } from "next-themes";
 import Image from "next/image";
-import SearchBox from "./components/search";
+import HomePage from './components/homePageData';
 import { ChangeTheme } from "./components/changeColor";
+import axios from "axios";
+import SearchBox from "./components/search";
 
-export default async function HomePage() {
-  // استفاده از fetch برای دریافت داده‌ها
-  const response = await axios.get("https://fa.wikipedia.org/w/api.php", {
-    params: {
-      action: "query",
-      list: "random",
-      rnlimit: "8",
-      format: "json",
-      origin: "*",
-      rnnamespace: 0
-    },
-    headers: {
-      'Cache-Control': 'no-store' // برای اطمینان از بارگذاری داده‌های تازه
-    }
-  });
 
-  const data = response.data.query.random;
+export default function Home() {
+  
 
   return (
     <div className="">
       <ChangeTheme />
       <Card>
         <SearchBox />
-        <div>
-          {data.map((item: any) => (
-            <Link key={item.id} href={`/${item.id}`}>
-              <h2>{item.title}</h2>
-            </Link>
-          ))}
-        </div>
+        <HomePage />
+
       </Card>
     </div>
   );
 }
-
-// مشخص کردن زمان به‌روز رسانی با ویژگی `revalidate`
-export const revalidate = 10; // صفحه هر 10 ثانیه یکبار به‌روز می‌شود
+export const revalidate = 10;
